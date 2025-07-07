20d524898b10de8f04eca06d8b81d069285e2fb3
Jeyms097 <jamespanaga213@gmail.com>
2025-07-08 01:55:15 +0800

    routes updated again

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
+// Route::get('/', function () { return view('main');});
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 Route::resource('tasks', TaskController::class)->except(['show']);
